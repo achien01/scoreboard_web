4a3e149b5ffa81b6515d3af594ed53597a10e4fd
yy1169695 <yy1169695@gmail.com>
2023-07-03 09:15:07 +0800

    修改樣式、旋轉、點擊範圍

--- src/App.js
+++ src/App.js
@@ -1,52 +1,128 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
+    const { innerWidth: width, innerHeight: height } = window;
+    return { width, height };
 }
 function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return windowDimensions;
+}
+const App = () => {
+    const { height } = useWindowDimensions();
+    useEffect(() => {
+        const handleResize = () => {
+            // 重新調整畫面尺寸
+            forceUpdate();
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    const forceUpdate = () => {
+        // 強制更新組件
+        setForceUpdate({});
+    };
+    const [teamAScore, setTeamAScore] = useState(0);
+    const [teamBScore, setTeamBScore] = useState(0);
+    const addScore = (team, point) => {
+        if (team === 'A') {
+            setTeamAScore(teamAScore + point);
+        } else if (team === 'B') {
+            setTeamBScore(teamBScore + point);
+        }
+    };
+    const isPortrait = window.innerHeight > window.innerWidth;
+    const containerDisplay = isPortrait ? 'block' : 'flex'
+    const boxSize = isPortrait ? { width: '100%', height: '50%' } : { width: '50%', height: '100%' };
+    const boxDisplay = isPortrait ? 'flex' : 'block'
+    const padSize = isPortrait ? { width: '50%', height: '100%' } : { width: '100%', height: '50%' };
+    const containerStyle = {
+        width: '100vw',
+        height: '100vh',
+        display: containerDisplay,
+    };
+    const boxStyle = {
+        width: boxSize.width,
+        height: boxSize.height,
+        display: boxDisplay,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row-reverse',
+    };
+    const padStyle = {
+        width: padSize.width,
+        height: padSize.height,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
     }
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-  return windowDimensions;
-}
-
-let padding = 5
-
-function App() {
-  const { height, width } = useWindowDimensions();
-  let box = { width: (width - padding * 3) / 2, height: (height - padding * 3) / 2 }
-  const [teamA, setTeamA] = useState(0);
-  const [teamB, setTeamB] = useState(0);
-  return (
-    <div style={{ width: width - padding * 2, height: height - padding * 2, padding: padding, background: 'gray', display: 'flex', justifyContent: 'space-between' }}>
-      <div style={{ width: box.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <p style={{ fontSize: width * 0.4, color: 'white' }}>{teamA}</p>
-      </div>
-      <div style={{ width: box.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <p style={{ fontSize: width * 0.4, color: 'white' }}>{teamB}</p>
-      </div>
-
-      <div onClick={() => { setTeamA(teamA + 1) }} style={{ position: 'absolute', top: padding, left: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
-      </div>
-      <div onClick={() => { teamA > 0 && setTeamA(teamA - 1) }} style={{ position: 'absolute', bottom: padding, left: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
-      </div>
-      <div onClick={() => { setTeamB(teamB + 1) }} style={{ position: 'absolute', top: padding, right: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
-      </div>
-      <div onClick={() => { teamB > 0 && setTeamB(teamB - 1) }} style={{ position: 'absolute', bottom: padding, right: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
-      </div>
-
-    </div>
-  );
-}
+    const scoreContainerStyle = {
+        position: 'absolute',
+        zIndex: -1,
+        width: '100vw',
+        height: '100vh',
+        display: containerDisplay,
+    }
+    const scoreBoxAStyle = {
+        width: boxSize.width,
+        height: boxSize.height,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#222831'
+    };
+    const scoreBoxBStyle = {
+        width: boxSize.width,
+        height: boxSize.height,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#30475E'
+    };
+    const scoreSize = isPortrait ? height * 0.5 / 2 : height * 0.5
+    const scoreStyle = {
+        fontSize: scoreSize, color: 'white',
+    }
+    const [_, setForceUpdate] = React.useState({});
+    return (
+        <div style={containerStyle}>
+            <div style={scoreContainerStyle}>
+                <div style={scoreBoxAStyle}>
+                    <span style={scoreStyle}>{teamAScore}</span>
+                </div>
+                <div style={scoreBoxBStyle}>
+                    <span style={scoreStyle}>{teamBScore}</span>
+                </div>
+            </div>
+            <div style={boxStyle}>
+                <div onClick={() => { addScore('A', +1) }} style={padStyle}>
+                    {/* <span style={textStyle}>A+</span> */}
+                </div>
+                <div onClick={() => { addScore('A', -1) }} style={padStyle}>
+                    {/* <span style={textStyle}>A-</span> */}
+                </div>
+            </div>
+            <div style={boxStyle}>
+                <div onClick={() => { addScore('B', +1) }} style={padStyle}>
+                    {/* <span style={textStyle}>B+</span> */}
+                </div>
+                <div onClick={() => { addScore('B', -1) }} style={padStyle}>
+                    {/* <span style={textStyle}>B-</span> */}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
